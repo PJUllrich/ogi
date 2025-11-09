30f184dc39dc676bf829964f3c6df2b4f2146bb8
Peter Ullrich <PJUllrich@users.noreply.github.com>
2025-11-09 16:45:02 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ogi.MixProject do
   def project do
     [
       app: :ogi,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       description: "Renders OpenGraph Images using Typst",
